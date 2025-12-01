20fe2dbf8e9835e7537d9974f69a1b0bfff07681
Akane230 <dagoc.kharljey@gmail.com>
2025-12-02 04:19:06 +0800

    Nothing changes here

--- src/models/Blotter.js
+++ src/models/Blotter.js
@@ -48,7 +48,7 @@ const attachmentSchema = new mongoose.Schema(
   { _id: false }
 );
 
-// Unified Blotter Schema
+// Unified Blotter Schemay
 const blotterSchema = new mongoose.Schema(
   {
     custom_id: {
